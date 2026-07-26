c6effe4b5a7260a32bd01dd2fbb10fd5cc58557c
Nikoloz Gvelesiani <nikolozgvelesiani10@gmail.com>
2026-07-26 22:31:09 +0400

    Hosting changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './i18n'
 import './index.css'
 import App from './App.jsx'
@@ -9,9 +9,9 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </StrictMode>,
 )
